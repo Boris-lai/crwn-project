47f6177e9cda8bd84538c99f4bf32bfae22fdd35
Boris <borislai@BorisdeMacBook-Pro.local>
2023-04-13 21:19:45 +0800

    created authentication page component alongside sign in form

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -4,6 +4,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 } from "firebase/auth";
 
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
@@ -67,5 +68,11 @@ export const createUserDocumentFromAuth = async (userAuth, addtionalInformation 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
   
-  return await createUserWithEmailAndPassword(auth, email, password)
+  return await createUserWithEmailAndPassword(auth, email, password) // firebase的函數
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  
+  return await signInWithEmailAndPassword(auth, email, password)  // firebase的函數
 }
